55b04c704e175957cb205e8d075df7b4a0251232
illemonati <tonymiaotong@tioft.tech>
2018-09-02 22:42:16 -0700

    Added Script, changed readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # http_getter
 A simple rust script using the rocket crate to get the status,headers, and response a http GET request
+
+Taken from https://github.com/hyperium/hyper/blob/master/examples/client.rs
+Added tls support taken from https://hyper.rs/guides/client/configuration/
---
+++ src/main.rs
@@ -0,0 +1,45 @@
+extern crate hyper;
+extern crate hyper_tls;
+
+use std::env;
+use std::io::{self, Write};
+use hyper::Client;
+use hyper::rt::{self, Future, Stream};
+use hyper_tls::HttpsConnector;
+
+
+fn main() {
+    let url = match env::args().nth(1) {
+        Some(url) => url,
+        None => {
+            println!("Usage: http_getter <url>");
+            return;
+        }
+    };
+    let url = url.parse::<hyper::Uri>().unwrap();
+    if (url.scheme_part().map(|s| s.as_ref()) != Some("https")) && (url.scheme_part().map(|s| s.as_ref()) != Some("http")) {
+        println!("http_getter only works with 'http/https' URLs.");
+        return;
+    }
+    rt::run(fetch_url(url));
+}
+
+fn fetch_url(url: hyper::Uri) -> impl Future<Item=(), Error=()> {
+    let https = HttpsConnector::new(4).expect("TLS initialization failed");
+    let client = Client::builder()
+        .build::<_, hyper::Body>(https);
+
+    client
+        .get(url)
+        .and_then(|res| {
+            println!("Response: {}", res.status());
+            println!("Headers: {:#?}", res.headers());
+            res.into_body().for_each(|chunk| {
+                io::stdout().write_all(&chunk)
+                    .map_err(|e| panic!("http_getter expects stdout is open, error={}", e))
+            })
+        })
+        .map_err(|err| {
+            eprintln!("Error {}", err);
+        })
+}
